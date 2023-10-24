c6b2cef4b4a9f623fbbac8cd59302ff93cd02bdc
Nikita.Zmanovskiy <72348175+test212366@users.noreply.github.com>
2023-10-24 21:53:10 +0300

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/akhil/slack-age-bot
+
+go 1.21.3
---
+++ main.go
@@ -0,0 +1,16 @@
+// package main
+
+// import (
+// 	"context"
+// 	"fmt"
+// 	"github.com/shomali11/slacker"
+// 	"log"
+// 	"os"
+// )
+
+// func main() {
+// 	os.Setenv("SLACK_BOT_TOKEN", "")
+
+// 	os.Setenv("SLACK_BOT_TOKEN", "")
+
+// }
